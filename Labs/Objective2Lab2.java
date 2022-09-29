b377f4f0bc7afa2bd9cda6038a77cb23c5bde9e3
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-09-29 14:34:28 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Name: Samuel Cassidy";
+    String age = "Age: 23";
+    String state = "State: Colorado";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
