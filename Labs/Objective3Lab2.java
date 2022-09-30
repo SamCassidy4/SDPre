3edc3ab701be3e754c7598a3d23366fd3590e5e2
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-09-30 15:03:29 -0600

    Complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 23;
+    side2 = 36;
+    double a = side1 * side1;
+    double b = side2 * side2;
+    double c = Math.sqrt(a + b);
+    System.out.println("Hypotenuse: " +c);
+  }
+}
