5cbeb9394e1ff0a3031489f62bbed5ff7e6c5390
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-10-05 12:47:47 -0600

    Complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String [] args) {
+
+    for (int num = 1; num <= 10; num = num + 1){
+    System.out.println(num);
+}
+    for (int num2 = 10; num2>1; num2 = num2 -1){
+    System.out.println(num2);
+}
+  }
+}
