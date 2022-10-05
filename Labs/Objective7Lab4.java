b1a23b3044f88c527f53a45b060be65cb24a698c
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-10-05 11:51:16 -0600

    Complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,10 @@
+public class Objective7Lab4 {
+  public static void main( String[] args) {
+    int count = 0;
+
+    for (int run = 1; run<=20; run = run +1){
+      count = count + run;
+    }
+    System.out.println( "The sum is " + count);
+    }
+  }
