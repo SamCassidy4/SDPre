937c0897d0ca72952a6e3ac00bd4e241db733975
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-10-05 12:51:49 -0600

    Complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int run = 1; run <=20; run = run +1){
+      sum = sum + run;
+    }
+    System.out.println(sum);
+    }
+  }
