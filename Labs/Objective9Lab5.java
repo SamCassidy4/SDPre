403a963665cd29657fae35e84551db2f4c53eb2f
Sam Cassidy <samuelcassidy1999@gmail.com>
2022-10-06 13:24:33 -0600

    Complete Objective9Lab5

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Objective9Lab5 {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner (System.in);
+    double num1, num2;
+    boolean keepGoing = true;
+    int choice;
+    double answer = 0.0;
+
+    System.out.println("Please enter a number: ");
+    num1 = kb.nextInt();
+    System.out.println("Please enter another number: ");
+    num2 = kb.nextInt();
+
+    while(keepGoing) {
+      printMenu();
+      System.out.print("Which would you like to do? ");
+      choice = kb.nextInt();
+
+      switch(choice) {
+        case 1:
+        answer = findSum(num1, num2);
+        System.out.println(num1 + "+" + num2 + " = " + answer);
+        break;
+
+        case 2:
+         answer = findAverage(num1, num2);
+        System.out.println("The average of " + num1 + " + " + num2 + " = " + answer);
+        break;
+
+        case 3:
+         answer = calcTax(num1, num2);
+        System.out.println("The amount in tax to be collected from a purchase of " + num1 + " and " + num2 + " is " + answer);
+        break;
+
+        case 4:
+        System.out.println("You have successfully exited.");
+        kb.close();
+        break;
+
+        default:
+        System.out.println("Invalid entry, please try again.");
+      }
+  }
+}
+  public static void printMenu() {
+      System.out.println();
+      System.out.println("========== MENU ==========");
+      System.out.println("|                         |");
+      System.out.println("|      1. Add Numbers     |");
+      System.out.println("|      2. Find Average    |");
+      System.out.println("|      3. Calculate Tax   |");
+      System.out.println("|      4. Exit            |");
+      System.out.println("|                         |");
+      System.out.println("===========================");
+      System.out.println();
+    }
+    public static double findSum(double x, double y) {
+      double sum = x + y;
+      return sum;
+    }
+    public static double findAverage(double x, double y) {
+      double average = (x + y)/2;
+      return average;
+    }
+    public static double calcTax(double x, double y) {
+      double tax = (x + y) * 0.0831;
+      return tax;
+    }
+  }
